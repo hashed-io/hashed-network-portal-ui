7497163db15486376bff3d7b8f82be71e07c57bc
garcia-alejandro <alejandro@hashed.io>
2022-08-02 18:34:11 -0500

    Removed unnecesary method

--- src/pages/marketplace/privacy/Index.vue
+++ src/pages/marketplace/privacy/Index.vue
@@ -236,19 +236,6 @@ export default defineComponent({
       this.getUploadResponse.payload = undefined
       this.getUploadResponse.type = undefined
     }
-    // async loginUser () {
-    //   try {
-    //     this.showLoading({ message: 'You must be logged in to submit an application' })
-    //     await this.$store.$hashedPrivateApi.login(this.selectedAccount.address)
-    //     this.setIsLoggedIn(true)
-    //   } catch (error) {
-    //     console.error(error)
-    //     this.showNotification({ message: error.message || error, color: 'negative' })
-    //     this.setIsLoggedIn(false)
-    //   } finally {
-    //     this.hideLoading()
-    //   }
-    // }
   }
 })
 </script>
