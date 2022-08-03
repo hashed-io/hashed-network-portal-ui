6f77a1d271d506e07788717fd05739f2113e775f
garcia-alejandro <alejandro@hashed.io>
2022-08-03 14:33:03 -0500

    Tax credit page

---
+++ src/pages/marketplace/TaxCredits/Index.vue
@@ -0,0 +1,22 @@
+<template lang='pug'>
+div component Index works!
+</template>
+<script>
+export default {
+  name: 'Index',
+  components: {
+
+  },
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+<style lang='styl'>
+
+</style>
