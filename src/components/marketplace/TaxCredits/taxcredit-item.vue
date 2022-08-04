2c11349259d4d8b6b8bb629f2ae55b28569689e0
garcia-alejandro <alejandro@hashed.io>
2022-08-04 16:31:18 -0500

    Created new component to show the tax credit info

---
+++ src/components/marketplace/TaxCredits/taxcredit-item.vue
@@ -0,0 +1,38 @@
+<template lang='pug'>
+q-card.q-pa-sm(v-ripple flat bordered @click="onClickTax")
+  .row.justify-center.q-py-md
+    q-icon(name="account_balance" size="4.1em" color="black")
+  #labels
+    .row.justify-evenly(v-for="{label, value} of unique")
+      div {{ label}}
+      div {{ value}}
+</template>
+<script>
+export default {
+  name: 'TaxcreditItem',
+  components: {
+
+  },
+  props: {
+    unique: {
+      type: Object,
+      default: undefined,
+      required: true
+    }
+  },
+  emits: ['onClick'],
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    onClickTax () {
+      this.$emit('onClick')
+    }
+  }
+}
+</script>
+<style lang='styl'>
+
+</style>
