5ff9472336455e46c2d614755b0bb68b4431b3ab
garcia-alejandro <alejandro@hashed.io>
2022-08-10 16:08:16 -0500

    Renamed the files

---
+++ src/components/marketplace/NFTs/NFT-form.vue
@@ -0,0 +1,89 @@
+<template lang="pug">
+#container
+  .text-h5.q-pb-lg {{$t('pages.marketplace.taxCredits.createFormTitle')}}
+  q-card(flat)
+    q-card-section
+      q-btn(
+        :label="$t('pages.marketplace.taxCredits.buttons.addAttribute')"
+        color="primary"
+        rounded
+        no-caps
+        icon="add"
+        @click="onAddAttribute"
+      ).q-my-lg
+      q-form(ref="taxCreditForm" @submit="onSubmitTaxCredit")
+        NFTInput(
+          v-for="(attribute, index) in attributes"
+          :key="index"
+          :index="index"
+          :rulesLabel="[rules.required]"
+          :rulesValue="[rules.required]"
+          v-model:label="attributes[index].label"
+          v-model:value="attributes[index].value"
+          @onRemoveAttribute="onRemoveAttribute"
+        )
+        q-btn.col-6.q-mt-md(
+          :label="$t('pages.marketplace.taxCredits.buttons.createUnique')"
+          type="submit"
+          color='primary'
+          icon="add"
+          rounded
+          no-caps)
+</template>
+<script>
+import { validation } from '~/mixins/validation'
+import NFTInput from '~/components/marketplace/NFTs/NFT-input.vue'
+export default {
+  name: 'NFTForm',
+  components: {
+    NFTInput
+  },
+  mixins: [validation],
+  emits: ['onSubmitTax'],
+  data () {
+    return {
+      attributes: [
+        {
+          label: undefined,
+          value: undefined
+        }
+      ]
+
+    }
+  },
+  methods: {
+    onRemoveAttribute (index) {
+      if (this.attributes.length > 1) {
+        this.attributes.splice(index, 1)
+      } else {
+        this.showNotification({ message: 'You must have at least one attribute', color: 'negative' })
+      }
+    },
+    onAddAttribute () {
+      this.attributes.push({
+        label: undefined,
+        value: undefined
+      })
+    },
+    async onSubmitTaxCredit () {
+      const isValid = await this.$refs.taxCreditForm.validate()
+      if (isValid) {
+        let containFile = false
+        const processedData = this.attributes.map(attribute => {
+          if (attribute.value instanceof File) {
+            containFile = true
+          }
+          return [
+            attribute.label,
+            attribute.value
+          ]
+        })
+        this.$emit('onSubmitTax', processedData, containFile)
+      }
+    }
+  }
+}
+</script>
+<style lang='stylus' scoped>
+
+</style>
---
+++ src/components/marketplace/NFTs/NFT-item.vue
@@ -0,0 +1,48 @@
+<template lang='pug'>
+q-card.q-pa-sm(v-ripple flat bordered @click="onClickTax")
+  q-card-section
+    .row.justify-center.q-py-md
+      q-icon(name="account_balance" size="4.1em" color="black")
+    .row.justify-center.q-py-md
+      q-chip.text-white.text-bold(color="primary") {{'Unique'}} {{unique.classId}}
+    #labels
+      .row.justify-between.q-gutter-xs
+        div
+          .text-subtitle2.q-px-md {{'Owner'}}
+          account-item(:address="unique.owner" size="1em" inherit flat shortDisplay)
+        div
+          .text-subtitle2.q-px-md {{'issuer'}}
+          account-item(:address="unique.issuer" size="1em" inherit flat shortDisplay)
+      //- .row.justify-evenly(v-for="attribute in unique.attributes")
+      //-   div.col-6 {{ attribute.attribute }}
+      //-   div.col-6 {{ attribute.value }}
+</template>
+<script>
+import AccountItem from 'src/components/common/account-item.vue'
+export default {
+  name: 'NFTItem',
+  components: {
+    AccountItem
+  },
+  props: {
+    unique: {
+      type: Object,
+      default: undefined,
+      required: true
+    }
+  },
+  emits: ['onClickItem'],
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    onClickTax () {
+      this.$emit('onClickItem', this.unique)
+    }
+  }
+}
+</script>
+<style lang='stylus' scoped>
+</style>
