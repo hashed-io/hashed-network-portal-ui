c4d5edb788b5715ef4a1feb0da08adfdf3c883f8
Jose <josemaria@hashed.io>
2022-08-15 16:36:07 -0500

    Refactor test on storybook

--- src/stories/Common/Login/passwod-confidential-docs.stories.js
+++ src/stories/Common/Login/passwod-confidential-docs.stories.js
@@ -35,7 +35,7 @@ export const Base = Template.bind({})
 Base.args = {
   ssoProvider: 'Google',
   ssoUserId: '123',
-  ssoImage: '',
+  ssoImage: 'https://cdn.icon-icons.com/icons2/2468/PNG/512/user_kids_avatar_user_profile_icon_149314.png',
   displayName: 'jmgayosso',
   ssoAccount: {}
 }
