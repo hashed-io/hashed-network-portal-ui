651f083acf3eb0f389ba421d2f30f666aef6ae46
Jose <josemaria@hashed.io>
2022-08-15 19:13:01 -0500

    Implemented document form with jest and test fail test on server

--- src/components/common/login/sso-account-item.vue
+++ src/components/common/login/sso-account-item.vue
@@ -5,19 +5,28 @@
       q-avatar(size="md")
         img(:src="profilePicture")
     q-item-section
-        q-item-label(lines="1") {{ displayName }}
+        q-item-label.text-white(lines="1") {{ displayName }}
     q-item-section(avatar)
         q-icon.q-ml-sm(name="arrow_drop_down" color="white")
 </template>
 
 <script>
+/**
+ * Component used to render sso account info logged
+ */
 export default {
   name: 'SSOAccountItem',
   props: {
+    /**
+     * Display name
+     */
     displayName: {
       type: String,
       default: undefined
     },
+    /**
+     * Profile picture url
+     */
     profilePicture: {
       type: String,
       default: undefined
