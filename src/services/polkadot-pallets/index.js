e7d27e960b2312f2345bf838c7c231196593029e
Jose <josemaria@hashed.io>
2022-09-07 13:41:40 -0500

    - Removed nbvPellete

--- src/services/polkadot-pallets/index.js
+++ src/services/polkadot-pallets/index.js
@@ -1,10 +1,8 @@
-import NbvStorageApi from './nbvStorageApi'
 import MarketplaceApi from './marketplaceApi'
 import FruniquesApi from './fruniquesApi'
 import UniquesApi from './uniquesApi'
 
 export {
-  NbvStorageApi,
   MarketplaceApi,
   FruniquesApi,
   UniquesApi
