324a32d036dc7cfd09a9f5a1b85e86eead6f6fd5
Jose <josemaria@hashed.io>
2022-10-06 13:17:51 -0500

    Added validation to hasPSBT on sign psbt button

--- src/pages/nbv/proposals/proposal-details.vue
+++ src/pages/nbv/proposals/proposal-details.vue
@@ -67,7 +67,7 @@
               icon="qr_code"
               no-caps
               @click="showSignPSBT"
-              :disabled="isOffchainError"
+              :disabled="isOffchainError && hasPsbt"
               v-if="!alreadySigned"
             )
             q-tooltip(v-if="isOffchainError") {{ validationMessage }}
