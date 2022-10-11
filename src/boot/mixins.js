f334c2ced5f26d4c07a86d12fbd566a6729bade6
Jose <josemaria@hashed.io>
2022-10-11 14:31:42 -0500

    Updated nbv-clien-api

--- src/boot/mixins.js
+++ src/boot/mixins.js
@@ -1,6 +1,7 @@
 /* eslint-disable dot-notation */
 // import Vue from 'vue'
 import notifications from '~/mixins/notifications'
+import errorHandler from '~/mixins/errorHandler'
 import { customIcons } from '~/mixins/icons'
 import TInput from '~/components/common/input/t-input.vue'
 import HInput from '~/components/common/input/h-input.vue'
@@ -8,6 +9,7 @@ import AccountInput from '~/components/common/account-input.vue'
 // import { validation } from '~/mixins/validation'
 export default async ({ app }) => {
   app.mixin(notifications)
+  app.mixin(errorHandler)
   app.mixin(customIcons)
   app.component('TInput', TInput)
   app.component('HInput', HInput)
--- src/mixins/errorHandler.js
+++ src/mixins/errorHandler.js
@@ -10,99 +10,104 @@ const errorMaps = [
   {
     message: 'The xpub has already been uploaded and taken by an account',
     code: 'XPubAlreadyTaken'
+  },
+  {
+    message: "The Account doesn't have an xpub",
+    code: 'XPubNotFound'
+  },
+  {
+    message: 'The user already has an xpub, try to remove it first',
+    code: 'UserAlreadyHasXpub'
+  },
+  {
+    message: 'The Xpub cant be removed/changed because a vault needs it',
+    code: 'XpubLinkedToVault'
+  },
+  {
+    message: "The generated Hashes aren't the same",
+    code: 'HashingError'
+  },
+  {
+    message: 'Found Invalid name on an additional field',
+    code: 'InvalidAdditionalField'
+  },
+  {
+    message: 'The vault threshold cannot be greater than the number of vault participants',
+    code: 'InvalidVaultThreshold'
+  },
+  {
+    message: 'A defined cosigner reached its vault limit',
+    code: 'SignerVaultLimit'
+  },
+  {
+    message: 'Too many cosigners',
+    code: 'ExceedMaxCosignersPerVault'
+  },
+  {
+    message: 'Vault not found',
+    code: 'VaultNotFound'
+  },
+  {
+    message: 'A vault needs at least 1 cosigner',
+    code: 'NotEnoughCosigners'
+  },
+  {
+    message: 'Only the owner of this vault can do this transaction',
+    code: 'VaultOwnerPermissionsNeeded'
+  },
+  {
+    message: 'Vault members cannot be duplicate',
+    code: 'DuplicateVaultMembers'
+  },
+  {
+    message: 'The account must participate in the vault to make a proposal or sign',
+    code: 'SignerPermissionsNeeded'
+  },
+  {
+    message: 'The vault has too many proposals',
+    code: 'ExceedMaxProposalsPerVault'
+  },
+  {
+    message: 'Proposal not found (id)',
+    code: 'ProposalNotFound'
+  },
+  {
+    message: 'The account must be the proposer to remove it',
+    code: 'ProposerPermissionsNeeded'
+  },
+  {
+    message: 'An identical proposal exists in storage',
+    code: 'AlreadyProposed'
+  },
+  {
+    message: 'The proposal was already signed by the user',
+    code: 'AlreadySigned'
+  },
+  {
+    message: 'The proposal is already finalized or broadcasted',
+    code: 'PendingProposalRequired'
+  },
+  {
+    message: "The proposal signatures need to surpass the vault's threshold",
+    code: 'NotEnoughSignatures'
+  },
+  {
+    message: 'The proposal has structural failures',
+    code: 'InvalidProposal'
+  },
+  {
+    message: 'This vault cant take proposals due to structural failures',
+    code: 'InvalidVault'
   }
-// {
-// message: The Account doesn't have an xpub
-//   code: XPubNotFound,
-// },
-// {
-// message: The user already has an xpub, try to remove it first
-//   code: UserAlreadyHasXpub,
-// },
-// {
-// message: The Xpub cant be removed/changed because a vault needs it
-//   code: XpubLinkedToVault,
-// },
-// {
-// message: The generated Hashes aren't the same
-//   code: HashingError,
-// },
-// {
-// message: Found Invalid name on an additional field
-//   code: InvalidAdditionalField,
-// },
-// {
-// message: The vault threshold cannot be greater than the number of vault participants
-//   code: InvalidVaultThreshold,
-// },
-// {
-// message: A defined cosigner reached its vault limit
-//   code: SignerVaultLimit,
-// },
-// {
-// message: Too many cosigners
-//   code: ExceedMaxCosignersPerVault,
-// },
-// {
-// message: Vault not found
-//   code: VaultNotFound,
-// },
-// {
-// message: A vault needs at least 1 cosigner
-//   code: NotEnoughCosigners,
-// },
-// {
-// message: Only the owner of this vault can do this transaction
-//   code: VaultOwnerPermissionsNeeded,
-// },
-// {
-// message: Vault members cannot be duplicate
-//   code: DuplicateVaultMembers,
-// },
-// {
-// message: The account must participate in the vault to make a proposal or sign
-//   code: SignerPermissionsNeeded,
-// },
-// {
-// message: The vault has too many proposals
-//   code: ExceedMaxProposalsPerVault,
-// },
-// {
-// message: Proposal not found (id)
-//   code: ProposalNotFound,
-// },
-// {
-// message: The account must be the proposer to remove it
-//   code: ProposerPermissionsNeeded,
-// },
-// {
-// message: An identical proposal exists in storage
-//   code: AlreadyProposed,
-// },
-// {
-// message: The proposal was already signed by the user
-//   code: AlreadySigned,
-// },
-// {
-// message: The proposal is already finalized or broadcasted
-//   code: PendingProposalRequired,
-// },
-// {
-// message: The proposal signatures need to surpass the vault's threshold
-//   code: NotEnoughSignatures,
-// },
-// {
-//   message: 'The proposal has structural failures'
-//     code: InvalidProposal,
-//   },
-// {
-//     message: 'This vault cant take proposals due to structural failures'
-//     code: InvalidVault,
-//  }
 ]
 
 const handlerError = (error) => {
-  console.error(error, errorMaps)
+  const message = error.message || error
+  const isErrorOnMaps = errorMaps.find(e => e.code === message)
+  if (isErrorOnMaps) {
+    console.error(isErrorOnMaps.message)
+  }
+  console.error(error, errorMaps, message)
 }
 
 export default {
