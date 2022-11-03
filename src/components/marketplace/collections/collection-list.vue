a644acd83c68e2ea9750dbdf005ec44c3a6ecb15
garcia-alejandro <alejandro@hashed.io>
2022-11-03 13:08:20 -0600

    Created new Collection components

---
+++ src/components/marketplace/collections/collection-list.vue
@@ -0,0 +1,50 @@
+<template lang='pug'>
+#container
+  #EmptyList(v-if="collectionList.length === 0")
+    .row
+      q-card.col-12(flat)
+        q-card-section
+          .flex.flex-center.text-h6.q-py-xl(data-testid='empty-list') {{$t('pages.marketplace.taxCredits.emptyList')}}
+  #content(v-else)
+    .row.justify-start.q-col-gutter-md
+      .col-3(v-for="(collection, key) in collectionList")
+        CollectionItem.tax-class.animated-item(
+          :key="key"
+          :collection="collection"
+          @onClickItem="onClickTax"
+        )
+</template>
+<script>
+import CollectionItem from '~/components/marketplace/collections/collection-item.vue'
+/**
+   * Component to get all the NFT associated with the logged account
+   */
+export default {
+  name: 'CollectionList',
+  components: {
+    CollectionItem
+  },
+  props: {
+    collectionList: {
+      type: Array,
+      default: () => [],
+      required: true
+    }
+  },
+  emits: ['onClickCollection'],
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    onClickTax (classId) {
+      this.$emit('onClickCollection', classId)
+    }
+  }
+}
+</script>
+<style lang='stylus' scoped>
+.tax-class
+  height: auto
+</style>
