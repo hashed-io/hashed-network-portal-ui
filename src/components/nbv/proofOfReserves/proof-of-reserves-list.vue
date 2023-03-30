18a676d0a07efc3ee172e1885bfff0a89e4bb95d
Jose Maria Gayosso <josemaria@hashed.io>
2023-03-30 10:20:38 -0600

    ✨ feat(proof-of-reserves-list.vue): add Proof of Reserves list component with props and emits.

---
+++ src/components/nbv/proofOfReserves/proof-of-reserves-list.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+#container
+  #empty(v-if="!proofOfReserves || proofOfReserves.length === 0")
+    q-card.q-pa-md
+      .text-body2.text-center {{ $t('pages.nbv.proofOfReserves.emptyProofOfReserves') }}
+//-   #list(v-if="proofOfReserves && proofOfReserves.length > 0")
+    //- proposal-item.full-width.q-mt-md(v-for="proposal in proofOfReserves" v-bind="proposal" @proposalClicked="proposalClicked")
+</template>
+
+<script setup>
+import { defineEmits, defineProps } from 'vue'
+import ProofOfReservesItem from '~/components/nbv/proofOfReserves/proof-of-reserves-item'
+
+const props = defineProps({
+  /**
+   * Proof of reserves list to render for proof-of-reserves-item component
+   */
+  proofOfReserves: {
+    type: Array,
+    default: () => []
+  }
+})
+
+const emits = defineEmits(['onProofOfReservesSelected'])
+
+function proofOfReservesClicked (data) {
+  /**
+   * Emit the data of selected proof of reserves
+   */
+  this.$emit('onProofOfReservesSelected', data)
+}
+</script>
