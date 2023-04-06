e2c5f2e652ea2ed473a60870e203a7533602e153
Jose Maria Gayosso <josemaria@hashed.io>
2023-04-06 09:42:02 -0600

    🎨 style(login/not-balance-card.vue): update contactUs link to enter.hashed.club 🌐 chore(i18n/en-US/index.js): update login.hashedClubAccessibility text to remove 'Contact to join.'

--- src/i18n/en-US/index.js
+++ src/i18n/en-US/index.js
@@ -99,10 +99,10 @@ export default {
     },
     login: {
       googleDriveError: 'An error has occurred trying to get access to Google Drive',
-      hashedClubAccessibility: 'Hashed Club is only accessible to those that hold HASH. Contact to join.',
+      hashedClubAccessibility: 'Hashed Club is only accessible to those that hold HASH.',
       yourHashedAddress: 'Your HASHED address',
       contactUs: 'Contact us',
-      getTokens: 'Get HASH token'
+      getTokens: 'Get HASH'
     },
     nbv: {
       appName: 'Bitcoin Vaults',
